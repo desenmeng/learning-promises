f76cbe0c67d5b2d48070804d11bdbe7f6dd54c1b
demohi <mds@xue.bi>
2016-04-27 21:39:26 +0800

    update

--- promisify.js
+++ promisify.js
@@ -2,11 +2,7 @@ module.exports = fn => {
   return (...args) => {
     return new Promise((resolve, reject) => {
       args.push((err, result, ...other) => {
-        if (err || args.length === 1) {
-          reject(err)
-          return
-        }
-        resolve(result, other)
+        err ? reject(err) : resolve(result, other)
       })
       fn.apply(this, args)
     })
