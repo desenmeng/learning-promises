f43777e5fa5aa5412aa70b9c69c4d39f79540c09
demohi <mds@xue.bi>
2016-05-10 16:31:09 +0800

    remove unused

--- fixture/delay.js
+++ fixture/delay.js
@@ -1,8 +1,7 @@
-const promisify = require('../promisify')
 module.exports = number => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(number);
-    }, 100);
+      resolve(number)
+    }, 100)
   })
 }
